1c0b3bfa9397b42fd440113c6bcbdc7de4d1a20f
Chelo <marzelo.aguirre@gmail.com>
2024-11-08 09:46:21 -0500

    Agregar ruta raíz para listar las rutas de la API y actualizar la configuración de Serverless

--- handler.js
+++ handler.js
@@ -11,5 +11,22 @@ app.use(express.json());
 
 // Route for your API
 app.use("/api/people", peopleRouter);
+app.get("/", (req, res) => {
+    res.json({
+        routes: [
+            {
+                path: "/api/people",
+                method: "GET",
+                description: "Get all people"
+            },
+            {
+                path: "/api/people",
+                method: "post",
+                description: "Create a person"
+            }
+        ]
+    });
+});
+
 
 export const handler = serverless(app);
